47e5e0cc234704d4413a3f0361ce57e2cd4b9a4c
Nishank Phulera <nishank.phulera@genuinemark.org>
2024-03-19 15:18:18 +0530

    changes

--- android/app/src/main/java/com/ozoneoverseas/MainActivity.java
+++ android/app/src/main/java/com/ozoneoverseas/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "Ozone Overseas";
+    return "tibcon";
   }
   @Override
   protected void onCreate(Bundle savedInstanceState) {
--- ios/tibconTests/tibconTests.m
+++ ios/tibconTests/tibconTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface OzoneOverseasTests : XCTestCase
+@interface tibconTests : XCTestCase
 
 @end
 
-@implementation OzoneOverseasTests
+@implementation tibconTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
